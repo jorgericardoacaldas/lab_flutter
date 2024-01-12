94158d03b2f9bbfbda19f327eeaf1974b1878336
Jorge Ricardo <ricardo10caldas@gmail.com>
2024-01-12 19:59:06 -0300

    create class Transfers

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -3,15 +3,26 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(title: Text('Transferências'),),
-    body: Column(
+    body: ListaTransferencias(),
+    floatingActionButton: FloatingActionButton(
+  onPressed: () { },
+      child: Icon(Icons.add),
+    ),
+  ),
+));
+
+class ListaTransferencias extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return Column(
       children: <Widget> [
-        Card(
-        child: ListTile(
-          leading: Icon(Icons.monetization_on),
-          title: Text('100.0'),
-          subtitle: Text('1000'),
-        ),
-      ),
+        ItemTransferencia(Transferencia(100.00, 1000)),
+        ItemTransferencia(Transferencia(100.00, 1000)),
+        ItemTransferencia(Transferencia(100.00, 1000)),
+        ItemTransferencia(Transferencia(100.00, 1000)),
+        ItemTransferencia(Transferencia(100.00, 1000)),
+        ItemTransferencia(Transferencia(100.00, 1000)),
          Card(
         child: ListTile(
           leading: Icon(Icons.monetization_on),
@@ -20,10 +31,30 @@ void main() => runApp(MaterialApp(
         ),
       ),
       ],
-    ),
-    floatingActionButton: FloatingActionButton(
-  onPressed: () { },
-      child: Icon(Icons.add),
-    ),
-  ),
-));
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numeroConta.toString()),
+    ));
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}

7e893d61f3b791c54b991be61b07595e69928ecb
Jorge Ricardo <ricardo10caldas@gmail.com>
2024-01-12 19:42:26 -0300

    Add lines with transfers done

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -3,6 +3,24 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(title: Text('Transferências'),),
+    body: Column(
+      children: <Widget> [
+        Card(
+        child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text('100.0'),
+          subtitle: Text('1000'),
+        ),
+      ),
+         Card(
+        child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text('200.0'),
+          subtitle: Text('1000'),
+        ),
+      ),
+      ],
+    ),
     floatingActionButton: FloatingActionButton(
   onPressed: () { },
       child: Icon(Icons.add),

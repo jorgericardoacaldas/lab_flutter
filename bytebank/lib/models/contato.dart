3edd3dadafcaa4bf31f18c1775bb3d0a34063a0e
Jorge Ricardo <ricardo10caldas@gmail>
2024-01-15 23:46:17 -0300

    Contact List refactor

---
+++ bytebank/lib/models/contato.dart
@@ -0,0 +1,7 @@
+class Contato{
+  late final String nome;
+  late final int numeroConta;
+
+  Contato(this.nome, this.numeroConta);
+  
+}
--- bytebank/lib/screens/contacts.dart
+++ bytebank/lib/screens/contacts.dart
@@ -1,23 +1,59 @@
+import 'package:bytebank/models/contato.dart';
+import 'package:bytebank/screens/lista_transferencia.dart';
 import 'package:flutter/material.dart';
 
-class ContactsList extends StatelessWidget {
-  const ContactsList({ Key? key }) : super(key: key);
+class ContactsList extends StatefulWidget {
+  final List<Contato> _contatos = [];
+
+  
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return ContactsListState();
+  }
+}
+
+class ContactsListState extends State<ContactsList>{
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Contatos'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text('Alex'),
-              subtitle: Text('1000'),
-            ),
-          )
-        ],
+      body: ListView.builder(
+        itemCount: widget._contatos.length,
+        itemBuilder: (BuildContext context, int index) {
+          final _contato =  widget._contatos[index];
+          return itemContact(contato: _contato);
+        }     
       ) ,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context){
+              return ListaTransferencias();
+          }));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+}
+
+class itemContact extends StatelessWidget {
+  final Contato contato;
+  const itemContact({
+    super.key, required this.contato,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(contato.nome),
+        subtitle: Text(contato.numeroConta.toString()),
+      ),
     );
   }
 }
